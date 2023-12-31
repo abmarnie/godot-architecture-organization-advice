f28f006a235ca51bbe9c763456d35785da70581b
abmarnie <69956083+abmarnie@users.noreply.github.com>
2023-12-31 10:53:01 -0600

    Create default.gd

---
+++ default.gd
@@ -0,0 +1,13 @@
+```
+# meta-name: Default
+# meta-description: Custom base template for Node
+# meta-default: true
+
+extends  _BASE_
+
+func  _ready()  ->  void:
+	 pass
+
+func  _physics_process(delta: float)  ->  void:
+	 pass
+```
