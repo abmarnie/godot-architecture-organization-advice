f7ec0c311bd2672411e6490cac9b527ba5e4ab01
abmarnie <69956083+abmarnie@users.noreply.github.com>
2023-12-31 10:53:34 -0600

    Create Default.cs

---
+++ Default.cs
@@ -0,0 +1,21 @@
+// meta-name: Default
+// meta-description: Custom base template for Node
+// meta-default: true
+
+using _BINDINGS_NAMESPACE_;
+using System;
+
+namespace MyDefaultNamespace;
+
+public partial class _CLASS_ : _BASE_
+{
+    public override void _Ready()
+    {
+	    
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+	    
+    }
+}
